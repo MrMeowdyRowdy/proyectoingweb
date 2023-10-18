6c6f7cb2bc1f4cb63a0272c92e72a170a6c83996
Dennis <dennisocana@gmail.com>
2023-10-17 21:03:44 -0500

    fix migrations

--- web/database/migrations/2023_10_18_004805_create_contracts_table.php
+++ web/database/migrations/2023_10_18_004805_create_contracts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer("clientId");
             $table->date("creation");
             $table->float("price");
+            $table->timestamps();
 
         });
     }
