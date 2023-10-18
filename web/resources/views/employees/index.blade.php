c8b649ac10622e0180b15c8964d81dda6f6f06bf
Dennis <dennisocana@gmail.com>
2023-10-17 20:38:49 -0500

    creacion de views para employees

---
+++ web/resources/views/employees/index.blade.php
@@ -0,0 +1,67 @@
+@extends('employees.layouts')
+
+@section('content')
+
+<div class="row justify-content-center mt-3">
+    <div class="col-md-12">
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success" role="alert">
+                {{ $message }}
+            </div>
+        @endif
+
+        <div class="card">
+            <div class="card-header">Lista de Productos</div>
+            <div class="card-body">
+                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Añadir un nuevo producto</a>
+                <table class="table table-striped table-bordered">
+                    <thead>
+                      <tr>
+                        <th scope="col">S#</th>
+                        <th scope="col">Código</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acción</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($products as $product)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $product->code }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->quantity }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-warning btn-sm"><i class="bi bi-eye"></i> Mostrar</a>
+
+                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Editar</a>   
+
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="bi bi-trash" onclick="return confirm('Deseas eliminar este producto?');"></i> Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @empty
+                            <td colspan="6">
+                                <span class="text-danger">
+                                    <strong>No se ha encontrado productos</strong>
+                                </span>
+                            </td>
+                        @endforelse
+                    </tbody>
+                  </table>
+
+                  {{ $products->links() }}
+
+            </div>
+        </div>
+    </div>    
+</div>
+    
+@endsection
